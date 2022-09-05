de57cd0062ab071fd7f13ad99ef004dbc0819300
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-09-05 15:31:42 +0200

    Finish tut 1 & 2

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    /**
+     * this is a test
+     */
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {ID}");
+        }
+    }
+}
